4919dd79470dba58ab0bdc02c66ff1c87dbdffa5
Peter Sacco <peter.sacco@competec.ch>
2016-06-23 09:10:32 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+
+echo "Hello World - It's me!";
+
+//require_once __DIR__ . '/bootstrap.php';
+//
+//$dom = new \TheSeer\fDOM\fDOMDocument();
+//$dom->load(__DIR__.'/templates/formular.xsl');
+//
+//$renderer = new XslRenderer($dom, new \TheSeer\fXSL\fXSLTProcessor());
+//
+//$dataModel = new \TheSeer\fDOM\fDOMDocument();
+//$dataModel->load('prototypes/formValidation.xml');
+//
+//echo $renderer->render($dataModel);
