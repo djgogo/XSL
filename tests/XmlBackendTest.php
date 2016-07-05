19236949f6fc270381fc65c051b34fc61f725a73
Peter Sacco <peter.sacco@competec.ch>
2016-07-05 09:27:41 +0200

    Unit Tests

--- tests/XmlBackendTest.php
+++ tests/XmlBackendTest.php
@@ -24,52 +24,68 @@ class XmlBackendTest extends PHPUnit_Framework_TestCase
      * @var array
      */
     private $data;
-    /**
-     * @var PHPUnit_Framework_MockObject_MockObject
-     */
-    private $fDom;
 
     public function setUp()
     {
-        $this->filePath = '/var/www/bibliothek.competec.ch/prototypes/books.xml';
+        $this->filePath = __DIR__.'/../tests/test.xml';
         $this->fileBackend = $this->getMockBuilder(FileBackend::class)->disableOriginalConstructor()->getMock();
         $this->xmlBackend = new XmlBackend($this->filePath, $this->fileBackend);
-        $this->fDom = $this->getMockBuilder(\TheSeer\fDOM\fDOMDocument::class)->disableOriginalConstructor()->getMock();
 
         $this->data = [
             'author' => 'Autor',
-            'title' => 'Buchtitel',
+            'title' => 'Neues Buch',
             'genre' => 'Computer',
             'price' => '19.90',
             'publishDate' => '2016-12-12',
-            'description' => 'Beschreibung'
+            'description' => 'Neues Buch hinzufuegen'
         ];
     }
 
-    public function testXmlStringCanBeWritten()
+    public function testDataCanBeWrittenToXmlFile()
     {
         $xmlString = '<?xml version="1.0"?>
-            <catalog>
-                <book id="bk120">
-                <author>Test</author>
-                <title>Testtitel</title>
-                <genre>Computer</genre>
-                <price>50.00</price>
-                <publish_date>2016-12-12</publish_date>
-                <description>Beschreibung</description>
-                </book>
-            </catalog>';
+<catalog>
+   <book id="bk120">
+   <author>Test</author>
+   <title>TestTitel</title>
+   <genre>Romance</genre>
+   <price>50.00</price>
+   <publish_date>2016-02-01</publish_date>
+   <description>Bestehendes Buch</description>
+   </book>
+</catalog>';
 
+        $expectedOutput = '<?xml version="1.0"?>
+<catalog>
+  <book id="bk120">
+    <author>Test</author>
+    <title>TestTitel</title>
+    <genre>Romance</genre>
+    <price>50.00</price>
+    <publish_date>2016-02-01</publish_date>
+    <description>Bestehendes Buch</description>
+  </book>
+  <book id="bk121">
+    <author>Autor</author>
+    <title>Neues Buch</title>
+    <genre>Computer</genre>
+    <price>19.90</price>
+    <publish_date>2016-12-12</publish_date>
+    <description>Neues Buch hinzufuegen</description>
+  </book>
+</catalog>
+';
+        
         $this->fileBackend
             ->expects($this->exactly(2))
             ->method('load')
             ->with($this->filePath)
             ->willReturn($xmlString);
 
-//        $this->fDom
-//            ->expects($this->once())
-//            ->method('loadXML')
-//            ->with($xmlString);
+        $this->fileBackend
+            ->expects($this->once())
+            ->method('save')
+            ->with($this->filePath, $expectedOutput);
 
         $this->xmlBackend->writeDataToXml($this->data);
         $this->assertXmlStringEqualsXmlString($xmlString, $this->fileBackend->load($this->filePath));
