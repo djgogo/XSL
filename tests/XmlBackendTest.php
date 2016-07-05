95b988384877dc5a3436c82b61ae6cc61a44af55
Peter Sacco <peter.sacco@competec.ch>
2016-07-05 11:12:45 +0200

    Unit Test XmlBackendTest Refactored

--- tests/XmlBackendTest.php
+++ tests/XmlBackendTest.php
@@ -27,7 +27,7 @@ class XmlBackendTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->filePath = __DIR__.'/../tests/test.xml';
+        $this->filePath = __DIR__.'/test.xml';
         $this->fileBackend = $this->getMockBuilder(FileBackend::class)->disableOriginalConstructor()->getMock();
         $this->xmlBackend = new XmlBackend($this->filePath, $this->fileBackend);
 
@@ -43,38 +43,8 @@ class XmlBackendTest extends PHPUnit_Framework_TestCase
 
     public function testDataCanBeWrittenToXmlFile()
     {
-        $xmlString = '<?xml version="1.0"?>
-<catalog>
-   <book id="bk120">
-   <author>Test</author>
-   <title>TestTitel</title>
-   <genre>Romance</genre>
-   <price>50.00</price>
-   <publish_date>2016-02-01</publish_date>
-   <description>Bestehendes Buch</description>
-   </book>
-</catalog>';
-
-        $expectedOutput = '<?xml version="1.0"?>
-<catalog>
-  <book id="bk120">
-    <author>Test</author>
-    <title>TestTitel</title>
-    <genre>Romance</genre>
-    <price>50.00</price>
-    <publish_date>2016-02-01</publish_date>
-    <description>Bestehendes Buch</description>
-  </book>
-  <book id="bk121">
-    <author>Autor</author>
-    <title>Neues Buch</title>
-    <genre>Computer</genre>
-    <price>19.90</price>
-    <publish_date>2016-12-12</publish_date>
-    <description>Neues Buch hinzufuegen</description>
-  </book>
-</catalog>
-';
+        $xmlString = file_get_contents(__DIR__.'/xmlString.xml');
+        $expectedOutput = file_get_contents(__DIR__.'/expectedXmlOutput.xml');
         
         $this->fileBackend
             ->expects($this->exactly(2))
@@ -88,6 +58,6 @@ class XmlBackendTest extends PHPUnit_Framework_TestCase
             ->with($this->filePath, $expectedOutput);
 
         $this->xmlBackend->writeDataToXml($this->data);
-        $this->assertXmlStringEqualsXmlString($xmlString, $this->fileBackend->load($this->filePath));
+        $this->assertXmlStringEqualsXmlFile(__DIR__.'/xmlString.xml', $this->fileBackend->load($this->filePath));
     }
 }
